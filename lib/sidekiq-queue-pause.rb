d839f7a8ca17034d106be316d157cba40b56bf9b
Joshua McGee <jmcgee@ampion.net>
2024-09-09 11:13:07 -0400

    BAP-2808-2 Trying a syntax tweak

--- lib/sidekiq-queue-pause.rb
+++ lib/sidekiq-queue-pause.rb
@@ -18,7 +18,7 @@ module Sidekiq
       end
 
       def pause(queue, pkey = nil)
-        Sidekiq.redis { |it| it.set rkey(queue, pkey), "true" }
+        Sidekiq.redis { |it| it.set rkey(queue, pkey), 1 }
       end
 
       def unpause(queue, pkey = nil)
